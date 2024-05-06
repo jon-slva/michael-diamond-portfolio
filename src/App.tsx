ffc5f8006857613957285c47dcfd81d796153bc3
Jonathan Silva <jonathan.silva167@gmail.com>
2024-05-06 00:48:59 -0400

    Setup browser router, add homepage, do basic layout of components as they will appear on homepage, import styles to homepage, import reset.scss to main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import './App.scss'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Home } from './pages/Home/Home'
+
 
 function App() {
 
 	return (
-		<>
+		<BrowserRouter>
 
-		</>
+			<Routes>
+				<Route path='/' element={<Home />} />
+			</Routes>
+		</BrowserRouter>
 	)
 }
 
