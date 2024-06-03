9e3092b5c66eb3535729540990747ad8379fe49f
Jonathan Silva <jonathan.silva167@gmail.com>
2024-06-02 21:33:29 -0400

    Create JSON data for slider and gallery images, refactor where data imports occur to lower level components, leave commented out until clear that change is permanent

--- src/components/GalleryCategoryFilter/GalleryCategoryFilter.tsx
+++ src/components/GalleryCategoryFilter/GalleryCategoryFilter.tsx
@@ -1,10 +1,14 @@
 import GalleryGrid from '../GalleryGrid/GalleryGrid'
 import './GalleryCategoryFilter.scss'
-import { GalleryProps } from './types'
+// import { GalleryProps } from './types'
 import { ReactElement } from 'react'
 
+import galleryData from '../../data/portfolio_images.json'
 
-export function GalleryCategoryFilter({ galleryImages }: GalleryProps): ReactElement {
+
+export function GalleryCategoryFilter(
+	// { galleryImages }: GalleryProps
+): ReactElement {
 
 
 	return (
@@ -13,7 +17,7 @@ export function GalleryCategoryFilter({ galleryImages }: GalleryProps): ReactEle
 				{/* Logic for getting categories from data array & rendering them goes here */}
 			</aside>
 
-			<GalleryGrid galleryImages={galleryImages} />
+			<GalleryGrid galleryImages={galleryData} />
 
 		</section>
 	)
