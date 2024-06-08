8d4875cac627a9a0bce2e16126a1c02bde207cf3
Jonathan Silva <jonathan.silva167@gmail.com>
2024-06-08 18:09:16 -0400

    - Refactor GalleryGrid component to use thumbnail source instead of full image source - Edit images for web - Add thumbnails - Compress unedited images

--- src/components/GalleryGrid/GalleryGrid.tsx
+++ src/components/GalleryGrid/GalleryGrid.tsx
@@ -7,13 +7,13 @@ function GalleryGrid({ galleryImages }: GalleryProps): ReactElement {
 
 	return (
 		<section className='gallery-grid'>
-			
+
 			{galleryImages.map((image, index) => {
 				return (
 					<img
 						key={index}
 						className="gallery-grid__item"
-						src={image.url}
+						src={image.thumbSrc}
 						alt={image.alt}
 					/>
 				)
